07d6ba9c2e3d39eb8f88336e81eea6fc5c1790a9
mlukicdesign <mlukicdesign@gmail.com>
2025-12-27 17:41:15 +0800

    remove error

--- src/components/partials/navigation.tsx
+++ src/components/partials/navigation.tsx
@@ -24,7 +24,7 @@ export async function Navigation() {
             <Link href={link.href ? link.href : "#"}>{link.label}</Link>
 
             {/* Dropdown for child links */}
-            {link.children && link.children.length > 0 && (
+            {link.children && (
               <ul className="absolute left-0 top-full hidden group-hover:block bg-white border border-gray-200 rounded-md shadow-lg min-w-max z-50">
                 {link.children.map((childLink, index) => (
                   <li key={index} className="hover:bg-gray-100">
