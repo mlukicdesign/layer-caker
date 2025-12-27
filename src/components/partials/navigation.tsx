887a87e351999c0e63e50fb52545ce9107115307
mlukicdesign <mlukicdesign@gmail.com>
2025-12-27 17:56:46 +0800

    fixed type issues

--- src/components/partials/navigation.tsx
+++ src/components/partials/navigation.tsx
@@ -1,6 +1,7 @@
-import { NAVIGATION_QUERY } from "@/sanity/lib/queries";
+import { NAVIGATION_QUERYResult } from "@/sanity/types";
 import Link from "next/link";
 import { sanityFetch } from "@/sanity/lib/live";
+import { NAVIGATION_QUERY } from "@/sanity/lib/queries";
 
 export async function Navigation() {
   const { data: navigation } = await sanityFetch({
@@ -24,7 +25,7 @@ export async function Navigation() {
             <Link href={link.href ? link.href : "#"}>{link.label}</Link>
 
             {/* Dropdown for child links */}
-            {link.children && (
+            {link.children && link.children.length > 0 && (
               <ul className="absolute left-0 top-full hidden group-hover:block bg-white border border-gray-200 rounded-md shadow-lg min-w-max z-50">
                 {link.children.map((childLink, index) => (
                   <li key={index} className="hover:bg-gray-100">
